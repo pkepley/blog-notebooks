bfb409d504480f5fea5d73d6acb0a1b035057fb8
Paul Kepley <pakepley@gmail.com>
2019-07-28 17:50:26 -0500

    Function to plot the border connectivity graph.

--- 20190726_RiddlerSoln/solution.py
+++ 20190726_RiddlerSoln/solution.py
@@ -4,9 +4,10 @@ import zipfile
 import numpy as np
 import pandas as pd
 import geopandas as gpd
+from shapely.geometry import Point, LineString
 import matplotlib
 import matplotlib.pyplot as plt
-#matplotlib.use('Agg')
+
 
 def get_contiguous_states_list(data_path):
     df_cont_states = pd.read_csv('{0}/states.csv'.format(data_path))
@@ -15,7 +16,7 @@ def get_contiguous_states_list(data_path):
 
     return df_cont_states
 
-def get_contiguous_states(data_path):
+def get_contiguous_state_connections(data_path):
     # Pull connectivity data
     connectivity_data_path = '{0}/contiguous-usa.dat'.format(data_path)
     if not os.path.exists(connectivity_data_path):
@@ -31,12 +32,18 @@ def get_contiguous_states(data_path):
             contents = f.read()
 
     # Parse the contents to get 2 columns:
-    #   data[0][:] = base state
-    #   data[1][:] = border state
     state_connxns = [row.split(' ') for row in contents.split('\n')]
     state_connxns = np.array([row for row in state_connxns if len(row) > 1])
     df_state_connxns = pd.DataFrame(state_connxns, columns=['BASE_STATE', 'BORDER_STATE'])
-    
+
+    df_cont_states = get_contiguous_states_list(data_path)
+    df_state_connxns = pd.merge(df_state_connxns, df_cont_states, how='inner',
+                                left_on = 'BASE_STATE', right_on = 'STATE')
+    df_state_connxns.drop(columns='STATE', inplace=True)
+    df_state_connxns = pd.merge(df_state_connxns, df_cont_states, how='inner',
+                                left_on = 'BORDER_STATE', right_on = 'STATE')
+    df_state_connxns.drop(columns='STATE', inplace=True)
+   
     return df_state_connxns
 
 def get_contiguous_states_shapefile(data_path):
@@ -71,26 +78,64 @@ def get_contiguous_states_shapefile(data_path):
     df_cont_state_shps = pd.merge(df_cont_state_shps, df_cont_states,
                                   on = ['STATE', 'NAME'], how = 'inner')
 
+    return df_cont_state_shps
+
+def plot_connections(df_cont_state_shps, df_state_connxns, img_path=None):    
     # create a new data frame with just the centroids
     df_cont_state_shps['centroid'] = df_cont_state_shps.centroid
     df_state_centroids = df_cont_state_shps.drop('geometry', axis=1)
     df_state_centroids = df_state_centroids.set_geometry('centroid')
 
+    # plot the connections
+    df_plot_state_connxns = pd.merge(df_state_connxns,
+                                     df_state_centroids,
+                                     how = 'left',
+                                     left_on = ['BASE_STATE'],
+                                     right_on = ['STATE'],
+                                     suffixes=('','_base_state'))
+    df_plot_state_connxns = pd.merge(df_plot_state_connxns,
+                                     df_state_centroids,
+                                     how = 'left',
+                                     left_on = ['BORDER_STATE'],
+                                     right_on = ['STATE'],
+                                     suffixes=('','_border_state'))
+    df_plot_state_connxns.drop(['STATE', 'FIPS', 'LON', 'LAT', 'FIPS_border_state', 'LON_border_state', 'LAT_border_state', 'STATE_border_state'],
+                               axis=1, inplace=True)
+    df_plot_state_connxns = df_plot_state_connxns.rename(columns = {'centroid' : 'centroid_base_state', 'NAME' : 'NAME_border_state'})
+    df_plot_state_connxns['connecting_line'] = df_plot_state_connxns.apply(lambda x: LineString([x['centroid_base_state'], x['centroid_border_state']]), axis=1)
+    df_plot_state_connxns = gpd.GeoDataFrame(df_plot_state_connxns)
+    df_plot_state_connxns['geometry'] = df_plot_state_connxns['connecting_line']
+    df_plot_state_connxns.set_geometry('geometry')
+    
     # Plot
-    fig, ax = plt.subplots(figsize=(8,4))
+    fig, ax = plt.subplots()
     df_cont_state_shps.plot(ax=ax, color='white', edgecolor='black')
-    df_state_centroids.plot(ax=ax, markersize=10, color='C1')
+    df_plot_state_connxns.plot(ax=ax, zorder=1)
+    df_state_centroids.plot(ax=ax, markersize=10, color='C1', zorder=2)    
     plt.axis('off')
-    plt.savefig('./junk.png')
+
+    if img_path is not None:
+        plt.savefig('{0}/state_border_connections.png'.format(img_path),
+                    bbox_inches='tight', dpi=400)
+        
+
+    return fig, ax, df_plot_state_connxns
     
     
 if __name__ == '__main__':
     
     data_path = './data'
+    img_path  = './img'
     if not os.path.exists(data_path):
         os.mkdir(data_path)
+    if not os.path.exists(img_path):
+        os.mkdir(img_path)
 
     # Get data
-    get_contiguous_states_list(data_path)
-    get_contiguous_states(data_path)
-    get_contiguous_states_shapefile(data_path)
+    df_cont_states     = get_contiguous_states_list(data_path)
+    df_state_connxns   = get_contiguous_state_connections(data_path)
+    df_cont_state_shps = get_contiguous_states_shapefile(data_path)
+
+    # plot
+    if not os.path.exists('{0}/state_border_connections.png'.format(img_path)):
+        fig, ax, df_plot_state_connxns = plot_connections(df_cont_state_shps, df_state_connxns, img_path)
